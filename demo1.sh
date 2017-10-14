c3a6cc4033f27d7e99bc9e6b6f956c572d7359f4
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-10-14 16:01:25 -0700

    added a cmd to describe aws regions for ec2

--- demo1.sh
+++ demo1.sh
@@ -7,5 +7,9 @@ aws ec2 describe-instances --output table
 
 aws ec2 describe-instances --output json
 
+
 #Create tag and assign it to running instance
 aws ec2 create-tags --resources i-0b5c3557abf31fc2e --tags Key=Environment,Value=Production
+
+#Describe the regions where ec2 service is available
+aws ec2 describe-regions --output table
